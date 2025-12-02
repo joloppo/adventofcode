8f3e004f9c39ed5c92f1aed96ab03a7c3cbbacb7
Jolo- <lockomoto@googlemail.com>
2025-12-02 11:39:37 +0000

    Day 1 part 1

--- 2025/python/aoc/day1.py
+++ 2025/python/aoc/day1.py
@@ -1,5 +1,35 @@
 def solve(text: str) -> str:
-    return text
+    parsed = split_parse(text)
+    current_value = 50
+    current_counter = 0
+    for v in parsed:
+        current_value, current_counter = advance(current_value, current_counter, v)
+    return str(current_counter)
+
+
+def split_parse(text: str) -> list[int]:
+    lines = text.split()
+    out = []
+    for line in lines:
+        if not line:
+            continue
+        if line.startswith("L"):
+            out.append(-(int(line[1:])))
+            continue
+        if line.startswith("R"):
+            out.append(+(int(line[1:])))
+            continue
+        raise ValueError(f"Unexpected input: {line}")
+    return out
+
+
+def advance(current_value: int, counter: int, lr_value: int) -> tuple[int, int]:
+    """returns new current value and new counter"""
+    current_value += lr_value
+    current_value = current_value % 100
+    if current_value == 0:
+        counter += 1
+    return current_value, counter
 
 
 if __name__ == "__main__":
