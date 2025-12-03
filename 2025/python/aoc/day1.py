14af784933fc618b2108529b946605a7a4d7d9ce
Jolo- <lockomoto@googlemail.com>
2025-12-03 11:18:28 +0000

    Solve stuff

--- 2025/python/aoc/day1.py
+++ 2025/python/aoc/day1.py
@@ -3,7 +3,10 @@ def solve(text: str) -> str:
     current_value = 50
     current_counter = 0
     for v in parsed:
-        current_value, current_counter = advance(current_value, current_counter, v)
+        # current_value, current_counter = advance(current_value, current_counter, v)
+        current_value, current_counter = advance_part2(
+            current_value, current_counter, v
+        )
     return str(current_counter)
 
 
@@ -30,6 +33,21 @@ def advance(current_value: int, counter: int, lr_value: int) -> tuple[int, int]:
     if current_value == 0:
         counter += 1
     return current_value, counter
+
+
+def advance_part2(current_value: int, counter: int, lr_value: int) -> tuple[int, int]:
+    """returns new current value and new counter"""
+    new_value = current_value + lr_value
+    new_count = (abs(new_value - 50) + 50) // 100
+
+    # from zero going left
+    if new_value < 0 and current_value == 0:
+        new_count -= 1
+
+    counter += new_count
+    new_value = new_value % 100
+
+    return new_value, counter
 
 
 if __name__ == "__main__":
