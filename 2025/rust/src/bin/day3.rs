b3df472e2d02293c3ebe32e3b81ddefedd60a306
Jolo- <lockomoto@googlemail.com>
2025-12-25 16:13:31 +0000

    Use rstest

--- 2025/rust/src/bin/day3.rs
+++ 2025/rust/src/bin/day3.rs
@@ -1,5 +1,7 @@
 use aoc::harness;
 
+const N_DIGITS: usize = 12;
+
 fn main() {
     let day = harness::extract_day(file!());
     let input = harness::get_input(day);
@@ -9,7 +11,6 @@ fn main() {
 }
 
 fn max_line_for_joltage(input: &str) -> usize {
-    const N_DIGITS: usize = 12;
     let as_digits = input
         .chars()
         .filter_map(|v| v.to_digit(10))
@@ -23,10 +24,8 @@ fn max_line_for_joltage(input: &str) -> usize {
             .unwrap();
         current_ix += max_ix;
         let exponent = N_DIGITS - ix - 1;
-        dbg!(exponent);
         total += 10_usize.pow(exponent as u32) * (*max_value as usize);
     }
-    dbg!(input, total);
     return total;
 }
 
@@ -55,44 +54,19 @@ impl<U: Ord> FirstMax for [U] {
     }
 }
 
-/*
-def test_1():
-    input = "987654321111111"
-    result = solve(input)
-    assert result == str(987654321111)
-
-
-def test_2():
-    input = "811111111111119"
-    result = solve(input)
-    assert result == str(811111111119)
-
-
-def test_3():
-    input = "234234234234278"
-    assert solve(input) == str(434234234278)
-
-
-def test_4():
-    input = "818181911112111"
-    assert solve(input) == str(888911112111)
-*/
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn test_add() {
-        let input = "987654321111111";
-        let output = max_line_for_joltage(input);
-        assert_eq!(output,);
-    }
-
-    #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
+    #[rstest]
+    #[case(987654321111111, 987654321111)]
+    #[case(811111111111119, 811111111119)]
+    #[case(234234234234278, 434234234278)]
+    #[case(818181911112111, 888911112111)]
+    fn test1(#[case] input: usize, #[case] expected: usize) {
+        let input = input.to_string();
+        let output = max_line_for_joltage(&input);
+        assert_eq!(output, expected);
     }
 }
