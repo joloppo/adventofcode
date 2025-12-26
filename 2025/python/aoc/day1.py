b4635c2e12fd754170d2234c688f8e0bd846f706
StMiro <miro@kugut.de>
2025-12-26 14:24:30 +0000

    Solve day 1 part 1

--- 2025/python/aoc/day1.py
+++ 2025/python/aoc/day1.py
@@ -1,5 +1,28 @@
-def solve(text: str) -> str:
-    return text
+def solve(text: str) -> int:
+    position: int = 50
+
+    parts: list[str] = text.split()
+    count: int = 0
+    for part in parts:
+        part = turn_string_into_int(part)
+
+
+        position: int = position + part
+        position = position % 100
+        if position == 0:
+            count += 1
+
+        print(position)
+    return count
+
+def turn_string_into_int(row: str) -> int:
+    if row[0] == "R":
+        muliplier = 1
+    elif row[0] == "L":
+        muliplier = -1
+    else:
+        raise Exception("Something went wrong")
+    return muliplier*int(row[1:])
 
 
 if __name__ == "__main__":
